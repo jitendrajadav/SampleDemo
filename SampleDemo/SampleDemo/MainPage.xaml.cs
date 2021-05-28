d32a8311be2ff45803a1d5882275180b07d2c5c2
jitendra jadav <jitendra.jadav@outlook.com>
2021-05-28 19:53:35 +0530

    Add project files.

---
+++ SampleDemo/SampleDemo/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SampleDemo.ViewModel;
+using Xamarin.Forms;
+
+namespace SampleDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ SampleDemo/SampleDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using SampleDemo.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+
+namespace SampleDemo.ViewModel
+{
+    public class MainViewModel
+    {
+        private readonly HttpClient client;
+
+        public ObservableCollection<User> Users { get; set; }
+
+        public MainViewModel()
+        {
+            Users = new ObservableCollection<User>();
+            client = new HttpClient();
+            GetUsers();
+        }
+
+        private async void GetUsers()
+        {
+            try
+            {
+                var result = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+                if (result.IsSuccessStatusCode)
+                {
+                    var data = await result.Content.ReadAsStringAsync();
+                    Users = new ObservableCollection<User>(JsonConvert.DeserializeObject<IList<User>>(data));
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+        }
+    }
+}
